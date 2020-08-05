ab6240da8b680ed4989b1eb5dc490a5293e71d40
budiono15 <adebudiono7@gmail.com>
2020-08-05 07:35:55 +0700

    DB-First

---
+++ dobha.sql
@@ -0,0 +1,290 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost
+-- Generation Time: Aug 05, 2020 at 02:32 AM
+-- Server version: 10.4.13-MariaDB
+-- PHP Version: 7.2.31
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `dobha`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `barang`
+--
+
+CREATE TABLE `barang` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `kode_barang` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `nama` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `satuan_id` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `harga` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `kategori_id` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `barang`
+--
+
+INSERT INTO `barang` (`id`, `kode_barang`, `nama`, `satuan_id`, `harga`, `kategori_id`, `created_at`, `updated_at`) VALUES
+(1, 'DP-001', 'sdsds', '1', '212', '1', '2020-08-04 14:04:18', '2020-08-04 14:06:20');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `customer`
+--
+
+CREATE TABLE `customer` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `kode_customer` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `nama` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `alamat` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `telepon` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `kategori_id` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `customer`
+--
+
+INSERT INTO `customer` (`id`, `kode_customer`, `nama`, `alamat`, `telepon`, `kategori_id`, `created_at`, `updated_at`) VALUES
+(1, 'CD-001', 'ww', 'ww', '1', '1', '2020-08-04 12:01:29', '2020-08-04 12:01:29'),
+(2, 'CD-003', 'sss', 'sss', '111', '2', '2020-08-04 12:44:20', '2020-08-04 12:44:20'),
+(3, 'CD-004', 'xxx', 'xxx', '111', '2', '2020-08-04 13:49:34', '2020-08-04 13:49:34'),
+(4, 'CD-002', 'sag', 'sag', '1111', '1', '2020-08-04 12:50:52', '2020-08-04 12:50:52');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `kategori_customer`
+--
+
+CREATE TABLE `kategori_customer` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `kategori` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `kategori_customer`
+--
+
+INSERT INTO `kategori_customer` (`id`, `kategori`, `created_at`, `updated_at`) VALUES
+(1, 'Super Agen', NULL, NULL),
+(2, 'Agen', NULL, NULL),
+(3, 'Distributor', NULL, NULL),
+(4, 'Reseller', NULL, NULL),
+(5, 'Private', NULL, NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `migrations`
+--
+
+CREATE TABLE `migrations` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `migration` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `batch` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `migrations`
+--
+
+INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
+(5, '2019_08_19_000000_create_failed_jobs_table', 1),
+(6, '2020_07_29_132717_create_data_barang_table', 1),
+(32, '2014_10_12_000000_create_users_table', 2),
+(37, '2020_08_03_194141_create_customer_table', 3),
+(38, '2020_08_04_130810_create_barang_table', 4),
+(39, '2020_08_04_155312_create_satuan_table', 5),
+(40, '2020_08_04_164755_create_penjualan_tunai_table', 6),
+(41, '2020_08_04_171659_add_no_faktur_to_penjualan_tunai_table', 7),
+(42, '2020_08_04_172257_add_no_faktur_to_penjualan_tunai_table', 8),
+(44, '2020_08_04_172704_create_kategori_customer_table', 9);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `penjualan_tunai`
+--
+
+CREATE TABLE `penjualan_tunai` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `kode_customer_id` bigint(20) NOT NULL,
+  `tgl_transaksi` date NOT NULL,
+  `kode_barang_id` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `jumlah` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `total_harga` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `penjualan_tunai`
+--
+
+INSERT INTO `penjualan_tunai` (`id`, `kode_customer_id`, `tgl_transaksi`, `kode_barang_id`, `jumlah`, `total_harga`, `created_at`, `updated_at`) VALUES
+(1, 1, '2020-08-05', '1', '2', '11111', '2020-08-04 21:20:57', NULL),
+(3, 1, '2020-08-06', '1', '1', '1', '2020-08-04 16:51:15', '2020-08-04 16:51:15'),
+(4, 1, '2020-08-15', '1', '2', '2', '2020-08-04 16:52:37', '2020-08-04 16:52:37');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `satuan`
+--
+
+CREATE TABLE `satuan` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `satuan` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `satuan`
+--
+
+INSERT INTO `satuan` (`id`, `satuan`, `created_at`, `updated_at`) VALUES
+(1, 'BOX', NULL, '2020-08-04 09:44:25'),
+(2, 'PCS', '2020-08-04 09:16:17', '2020-08-04 10:25:22'),
+(3, 'LUSIN', '2020-08-04 10:25:00', '2020-08-04 10:25:00'),
+(4, 'KARTON', '2020-08-04 10:25:10', '2020-08-04 10:25:10');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `username` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email_verified_at` timestamp NULL DEFAULT NULL,
+  `password` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `remember_token` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `barang`
+--
+ALTER TABLE `barang`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `customer`
+--
+ALTER TABLE `customer`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `kategori_customer`
+--
+ALTER TABLE `kategori_customer`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `migrations`
+--
+ALTER TABLE `migrations`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `penjualan_tunai`
+--
+ALTER TABLE `penjualan_tunai`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `satuan`
+--
+ALTER TABLE `satuan`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `users_email_unique` (`email`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `barang`
+--
+ALTER TABLE `barang`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+
+--
+-- AUTO_INCREMENT for table `customer`
+--
+ALTER TABLE `customer`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=26;
+
+--
+-- AUTO_INCREMENT for table `kategori_customer`
+--
+ALTER TABLE `kategori_customer`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `migrations`
+--
+ALTER TABLE `migrations`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=45;
+
+--
+-- AUTO_INCREMENT for table `penjualan_tunai`
+--
+ALTER TABLE `penjualan_tunai`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `satuan`
+--
+ALTER TABLE `satuan`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
